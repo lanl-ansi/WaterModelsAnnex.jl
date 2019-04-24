374fa00017a6f291b327d16efd9e47af46485953
Byron Tasseff <byron@tasseff.com>
2019-04-24 17:49:35 -0600

    Trying to get sample MICP running.

--- src/alg/ne-no_good.jl
+++ src/alg/ne-no_good.jl
@@ -1,6 +1,6 @@
 """
 Implements a Benders-like algorithm that applies no-good cuts to network
-designs that are infeasible (with respect to flow and/or head bounds).
+designs that are infeasible with respect to flow and/or head bounds.
 """
 function ne_no_good(network_path::String, modifications_path::String,
                     nlp_optimizer::JuMP.OptimizerFactory,
@@ -9,16 +9,19 @@ function ne_no_good(network_path::String, modifications_path::String,
     network = WMs.parse_file(network_path)
     alpha = network["options"]["headloss"] == "h-w" ? 1.852 : 2.0
 
-    # Construct the CVXNLP feasibility model.
+    # Construct the CP feasibility model.
     wf = WMs.build_generic_model(network, WMs.CVXNLPWaterModel, WMs.get_post_wf(alpha))
 
-    # Add the modifications to the network data.
+    # Add the modifications to the network data and construct the MICP model.
     modifications = WMs.parse_file(modifications_path)
     InfrastructureModels.update_data!(network, modifications)
     ne = WMs.build_generic_model(network, WMs.MICPWaterModel, WMs.get_post_ne(alpha))
-
-    println(ne.model)
     optimize!(ne, mip_optimizer)
+    design_is_feasible = false
 
-    return true
+    #while !design_is_feasible
+    #    optimize!(ne, mip_optimizer)
+    #end
+
+    return design_is_feasible
 end
