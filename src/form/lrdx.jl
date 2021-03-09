3cba61b2f120e525f5ccc24f8327e59b0df27996
Byron Tasseff <btasseff@lanl.gov>
2021-03-08 19:37:47 -0700

    Add linear upper (or lower) bounds on nonlinear terms.

--- src/form/lrdx.jl
+++ src/form/lrdx.jl
@@ -1,3 +1,10 @@
+function _calc_pipe_flow_integrated_bound(q::JuMP.VariableRef, z::Union{JuMP.VariableRef, JuMP.GenericAffExpr}, q_lb::Float64, q_ub::Float64, exponent::Float64)
+    f_lb = 1.0 / (1.0 + exponent) * q_lb^(1.0 + exponent)
+    f_ub = 1.0 / (1.0 + exponent) * q_ub^(1.0 + exponent)
+    return f_lb * z + (f_ub - f_lb) / (q_ub - q_lb) * (q - q_lb * z)
+end
+
+
 function _calc_pipe_flow_integrated_oa(q::JuMP.VariableRef, z::Union{JuMP.VariableRef, JuMP.GenericAffExpr}, q_hat::Float64, exponent::Float64)
     f = 1.0 / (1.0 + exponent) * q_hat^(1.0 + exponent) * z
     return f + q_hat^exponent * (q - q_hat * z)
@@ -10,10 +17,24 @@ function _calc_pipe_head_integrated_oa(dh::JuMP.VariableRef, z::Union{JuMP.Varia
 end
 
 
+function _calc_pipe_head_integrated_bound(dh::JuMP.VariableRef, z::Union{JuMP.VariableRef, JuMP.GenericAffExpr}, dh_lb::Float64, dh_ub::Float64, exponent::Float64)
+    f_lb = exponent / (1.0 + exponent) * dh_lb^(1.0 + 1.0 / exponent)
+    f_ub = exponent / (1.0 + exponent) * dh_ub^(1.0 + 1.0 / exponent)
+    return f_lb * z + (f_ub - f_lb) / (dh_ub - dh_lb) * (dh - dh_lb * z)
+end
+
+
 function _calc_pump_flow_integrated_oa(q::JuMP.VariableRef, z::Union{JuMP.VariableRef, JuMP.GenericAffExpr}, q_hat::Float64, coeffs::Array{Float64, 1})
     f = (1.0 / 3.0) * coeffs[1] * q_hat^3 + 0.5 * coeffs[2] * q_hat^2 + coeffs[3] * q_hat
     df = coeffs[1] * q_hat^2 + coeffs[2] * q_hat + coeffs[3]
     return f * z + df * (q - q_hat * z)
+end
+
+
+function _calc_pump_flow_integrated_bound(q::JuMP.VariableRef, z::Union{JuMP.VariableRef, JuMP.GenericAffExpr}, q_lb::Float64, q_ub::Float64, coeffs::Array{Float64, 1})
+    f_lb = (1.0 / 3.0) * coeffs[1] * q_lb^3 + 0.5 * coeffs[2] * q_lb^2 + coeffs[3] * q_lb
+    f_ub = (1.0 / 3.0) * coeffs[1] * q_ub^3 + 0.5 * coeffs[2] * q_ub^2 + coeffs[3] * q_ub
+    return f_lb * z + (f_ub - f_lb) / (q_ub - q_lb) * (q - q_lb * z)
 end
 
 
@@ -22,6 +43,15 @@ function _calc_pump_gain_integrated_oa(g::JuMP.VariableRef, z::Union{JuMP.Variab
         (6.0 * c[1] * (g_hat - c[3]) + c[2]^2)) / (12.0 * c[1]^2)
     df = (sqrt(max(0.0, 4.0 * c[1] * (g_hat - c[3]) + c[2]^2)) + c[2]) / (2.0 * c[1])
     return f * z + df * (g - g_hat * z)
+end
+
+
+function _calc_pump_gain_integrated_bound(g::JuMP.VariableRef, z::JuMP.VariableRef, g_lb::Float64, g_ub::Float64, c::Array{Float64, 1})
+    f_lb = (max(0.0, (4.0 * c[1] * (g_lb - c[3]) + c[2]^2))^(1.5) + c[2] *
+        (6.0 * c[1] * (g_lb - c[3]) + c[2]^2)) / (12.0 * c[1]^2)
+    f_ub = (max(0.0, (4.0 * c[1] * (g_ub - c[3]) + c[2]^2))^(1.5) + c[2] *
+        (6.0 * c[1] * (g_ub - c[3]) + c[2]^2)) / (12.0 * c[1]^2)
+    return f_lb * z + (f_ub - f_lb) / (g_ub - g_lb) * (g - g_lb * z)
 end
 
 
@@ -62,8 +92,8 @@ end
 function variable_pump_gain_nonlinear(wm::AbstractLRDXModel; nw::Int=WM.nw_id_default, bounded::Bool=true, report::Bool=true)
     # Initialize variables associated with positive gain.
     g_nl = WM.var(wm, nw)[:g_nl_pump] = JuMP.@variable(
-        wm.model, [a in WM.ids(wm, nw, :pump)], lower_bound=0.0, base_name="$(nw)_g_nl",
-        start=WM.comp_start_value(WM.ref(wm, nw, :pump, a), "g_nl_start", 1.0e-6))
+        wm.model, [a in WM.ids(wm, nw, :pump)], base_name="$(nw)_g_nl",
+        start=WM.comp_start_value(WM.ref(wm, nw, :pump, a), "g_nl_start", 0.0))
 end
 
 
@@ -100,6 +130,12 @@ function constraint_pipe_flow_nonlinear(
         append!(WM.con(wm, n, :pipe_flow_nonlinear)[a], [c])
     end
 
+    if qp_min_forward < qp_ub
+        rhs = _calc_pipe_flow_integrated_bound(qp, y, qp_min_forward, qp_ub, exponent)
+        c = JuMP.@constraint(wm.model, qp_nl / L <= r * rhs)
+         append!(WM.con(wm, n, :pipe_flow_nonlinear)[a], [c])
+    end
+
     # Get variables for negative flow and head difference.
     qn, qn_nl = WM.var(wm, n, :qn_pipe, a), WM.var(wm, n, :qn_nl_pipe, a)
     qn_min_forward, qn_ub = max(0.0, -q_max_reverse), JuMP.upper_bound(qn)
@@ -113,6 +149,12 @@ function constraint_pipe_flow_nonlinear(
         c = JuMP.@constraint(wm.model, r * lhs <= qn_nl / L)
 
         # Append the :pipe_head_loss_integrated constraint array.
+        append!(WM.con(wm, n, :pipe_flow_nonlinear)[a], [c])
+    end
+
+    if qn_min_forward < qn_ub
+        rhs = _calc_pipe_flow_integrated_bound(qn, 1.0 - y, qn_min_forward, qn_ub, exponent)
+        c = JuMP.@constraint(wm.model, qn_nl / L <= r * rhs)
         append!(WM.con(wm, n, :pipe_flow_nonlinear)[a], [c])
     end
 end
@@ -144,6 +186,12 @@ function constraint_pipe_head_nonlinear(
         append!(WM.con(wm, n, :pipe_head_nonlinear)[a], [c])
     end
 
+    if dhp_lb < dhp_ub
+        rhs = _calc_pipe_head_integrated_bound(dhp, y, dhp_lb, dhp_ub, exponent)
+        c = JuMP.@constraint(wm.model, dhp_nl / L_r <= r_r * rhs)
+        append!(WM.con(wm, n, :pipe_head_nonlinear)[a], [c])
+    end
+
     # Get variables for positive flow and head difference.
     dhn, dhn_nl = WM.var(wm, n, :dhn_pipe, a), WM.var(wm, n, :dhn_nl_pipe, a)
     dhn_lb, dhn_ub = 0.0, JuMP.upper_bound(dhn)
@@ -157,6 +205,12 @@ function constraint_pipe_head_nonlinear(
         c = JuMP.@constraint(wm.model, r_r * lhs <= dhn_nl / L_r)
 
         # Append the :pipe_head_loss_integrated constraint array.
+        append!(WM.con(wm, n, :pipe_head_nonlinear)[a], [c])
+    end
+
+    if dhn_lb < dhn_ub
+        rhs = _calc_pipe_head_integrated_bound(dhn, 1.0 - y, dhn_lb, dhn_ub, exponent)
+        c = JuMP.@constraint(wm.model, dhn_nl / L_r <= r_r * rhs)
         append!(WM.con(wm, n, :pipe_head_nonlinear)[a], [c])
     end
 end
@@ -176,7 +230,7 @@ function constraint_on_off_pump_flow_nonlinear(
     qp_min_forward, qp_ub = max(0.0, q_min_forward), JuMP.upper_bound(qp)
 
     # Loop over breakpoints strictly between the lower and upper variable bounds.
-    for pt in range(qp_min_forward, stop = JuMP.upper_bound(qp), length = num_breakpoints)
+    for pt in range(qp_min_forward, stop = qp_ub, length = num_breakpoints)
         # Add a linear outer approximation of the convex relaxation at `pt`.
         rhs = _calc_pump_flow_integrated_oa(qp, z, pt, coeffs)
 
@@ -184,6 +238,12 @@ function constraint_on_off_pump_flow_nonlinear(
         c = JuMP.@constraint(wm.model, qp_nl <= rhs)
 
         # Append the :pump_head_loss_integrated constraint array.
+        append!(WM.con(wm, n, :on_off_pump_flow_nonlinear)[a], [c])
+    end
+
+    if qp_min_forward < qp_ub
+        lhs = _calc_pump_flow_integrated_bound(qp, z, qp_min_forward, qp_ub, coeffs)
+        c = JuMP.@constraint(wm.model, lhs <= qp_nl)
         append!(WM.con(wm, n, :on_off_pump_flow_nonlinear)[a], [c])
     end
 end
@@ -199,17 +259,25 @@ function constraint_on_off_pump_gain_nonlinear(
     z = WM.var(wm, n, :z_pump, a)
 
     # Get variables for positive flow and head difference.
-    g, g_nl = WM.var(wm, n, :g_pump, a), WM.var(wm, n, :g_nl_pump, a)
+    g = WM.var(wm, n, :g_pump, a)
+    g_nl = WM.var(wm, n, :g_nl_pump, a)
+    g_lb, g_ub = 0.0, JuMP.upper_bound(g)
 
     # Loop over breakpoints strictly between the lower and upper variable bounds.
-    for pt in range(0.0, stop = JuMP.upper_bound(g), length = num_breakpoints)
+    for pt in range(g_lb, stop = g_ub, length = num_breakpoints)
         # Add a linear outer approximation of the convex relaxation at `pt`.
-        lhs = _calc_pump_gain_integrated_oa(g, z, pt, coeffs)
+        rhs = _calc_pump_gain_integrated_oa(g, z, pt, coeffs)
 
         # Add outer-approximation of the integrated head loss constraint.
-        c = JuMP.@constraint(wm.model, lhs <= g_nl)
+        c = JuMP.@constraint(wm.model, g_nl <= rhs)
 
         # Append the :pump_head_loss_integrated constraint array.
+        append!(WM.con(wm, n, :on_off_pump_gain_nonlinear)[a], [c])
+    end
+
+    if g_lb < g_ub
+        lhs = _calc_pump_gain_integrated_bound(g, z, g_lb, g_ub, coeffs)
+        c = JuMP.@constraint(wm.model, -lhs <= g_nl) # TODO: Why negate lhs?
         append!(WM.con(wm, n, :on_off_pump_gain_nonlinear)[a], [c])
     end
 end
@@ -276,5 +344,8 @@ function constraint_strong_duality(wm::AbstractLRDXModel)
         end
     end
 
-    JuMP.@constraint(wm.model, pipe_nl - pump_nl + demand_sum - reservoir_sum + qh_nl_tank <= 0.0)
+    linear_terms = demand_sum - reservoir_sum
+    nonlinear_terms = pipe_nl - pump_nl - qh_nl_tank
+
+    JuMP.@constraint(wm.model, linear_terms + nonlinear_terms <= 0.0)
 end
