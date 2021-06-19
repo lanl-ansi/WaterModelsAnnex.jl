42f1593c1fa3d45fdf0260419fde12a9696dc89b
Byron Tasseff <btasseff@lanl.gov>
2021-06-19 09:59:40 -0600

    Change breakpoint functions.

--- src/alg/set_breakpoints.jl
+++ src/alg/set_breakpoints.jl
@@ -91,6 +91,49 @@ function set_breakpoints_piecewise!(network_mn::Dict{String, <:Any}, result_mn::
 end
 
 
+function set_breakpoints_accuracy!(data::Dict{String, <:Any}, outer_accuracy::Float64, inner_accuracy::Float64)
+    head_loss, viscosity = data["head_loss"], data["viscosity"]
+    base_length = get(data, "base_length", 1.0)
+    base_time = get(data, "base_time", 1.0)
+    exponent = uppercase(head_loss) == "H-W" ? 1.852 : 2.0
+
+    for pipe in values(data["pipe"])
+        node_fr = data["node"][string(pipe["node_fr"])]
+        node_to = data["node"][string(pipe["node_to"])]
+
+        dh_min = node_fr["head_min"] - node_to["head_max"]
+        dh_max = node_fr["head_max"] - node_to["head_min"]
+
+        r = WM._calc_pipe_resistance(pipe, head_loss,
+             viscosity, base_length, base_time)
+
+        if dh_max > dh_min && dh_max - dh_min > outer_accuracy
+            dh_range_outer = range(dh_min, dh_max, step = outer_accuracy)
+            q_range_outer = sign.(dh_range_outer) .* (abs.(dh_range_outer) ./
+                (pipe["length"] * r)).^(1.0 / exponent)
+            q_range_outer = filter(x -> pipe["flow_min"] < x &&
+                x < pipe["flow_max"], q_range_outer)
+            pipe["flow_lower_breakpoints"] = sort(vcat(q_range_outer,
+                [pipe["flow_min"], pipe["flow_max"]]))
+        else
+            pipe["flow_lower_breakpoints"] = [pipe["flow_min"], pipe["flow_max"]]
+        end
+
+        if dh_max > dh_min && dh_max - dh_min > inner_accuracy
+            dh_range_outer = range(dh_min, dh_max, step = inner_accuracy)
+            q_range_outer = sign.(dh_range_outer) .* (abs.(dh_range_outer) ./
+                (pipe["length"] * r)).^(1.0 / exponent)
+            q_range_outer = filter(x -> pipe["flow_min"] < x &&
+                x < pipe["flow_max"], q_range_outer)
+            pipe["flow_upper_breakpoints"] = sort(vcat(q_range_outer,
+                [pipe["flow_min"], pipe["flow_max"]]))
+        else
+            pipe["flow_upper_breakpoints"] = [pipe["flow_min"], pipe["flow_max"]]
+        end
+    end
+end
+
+
 function set_breakpoints_piecewise_degree!(network_mn::Dict{String, <:Any}, result_mn::Dict{String, <:Any})
     for nw_id in sort([parse(Int, x) for x in collect(keys(network_mn["nw"]))])[1:end-1]
         network = network_mn["nw"][string(nw_id)]
@@ -156,6 +199,8 @@ function set_breakpoints_heuristic!(network_mn::Dict{String, <:Any}, result_mn::
             pipe["flow_upper_breakpoints"] =
                 range(flow_min, flow_max; length = 10)
 
+            # println(nw_id, " ", i, " ", pipe["flow_lower_breakpoints"])
+
             # if degree_fr(network, pipe) + degree_to(network, pipe) <= 2
             #     if abs(result["pipe"][i]["q"]) > 1.0e-4
             #         flow_mid = max(flow_min, result["pipe"][i]["q"])
@@ -206,19 +251,21 @@ function set_breakpoints_heuristic!(network_mn::Dict{String, <:Any}, result_mn::
 end
 
 
-function set_breakpoints_num!(network::Dict{String, <:Any}, num_breakpoints::Int)
+function set_breakpoints_num!(network::Dict{String, <:Any}, num_outer_breakpoints::Int, num_inner_breakpoints::Int)
     for pipe in values(network["pipe"])
         flow_min, flow_max = pipe["flow_min"], pipe["flow_max"]
-        breakpoints = range(flow_min, flow_max; length = num_breakpoints)
-        pipe["flow_lower_breakpoints"] = breakpoints
-        pipe["flow_upper_breakpoints"] = breakpoints
+        inner_breakpoints = range(flow_min, flow_max; length = num_inner_breakpoints)
+        outer_breakpoints = range(flow_min, flow_max; length = num_outer_breakpoints)
+        pipe["flow_lower_breakpoints"] = outer_breakpoints
+        pipe["flow_upper_breakpoints"] = inner_breakpoints
     end
 
     for pump in values(network["pump"])
         flow_min, flow_max = pump["flow_min_forward"], pump["flow_max"]
-        breakpoints = range(flow_min, flow_max; length = num_breakpoints)
-        pump["flow_lower_breakpoints"] = breakpoints
-        pump["flow_upper_breakpoints"] = breakpoints
+        inner_breakpoints = range(flow_min, flow_max; length = num_inner_breakpoints)
+        outer_breakpoints = range(flow_min, flow_max; length = num_outer_breakpoints)
+        pump["flow_lower_breakpoints"] = inner_breakpoints
+        pump["flow_upper_breakpoints"] = outer_breakpoints
     end
 end
 
